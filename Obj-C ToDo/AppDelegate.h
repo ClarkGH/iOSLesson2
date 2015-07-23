95c8c5d88547a2d72b4c13418ae734bd9b88ec01
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-07-23 10:57:05 -0700

    Initial Commit

---
+++ Obj-C ToDo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Obj-C ToDo
+//
+//  Created by clark hinchcliff on 7/23/15.
+//  Copyright (c) 2015 Clark Hinchcliff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Obj-C ToDo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Obj-C ToDo
+//
+//  Created by clark hinchcliff on 7/23/15.
+//  Copyright (c) 2015 Clark Hinchcliff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
